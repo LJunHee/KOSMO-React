6a75737a431cafa03db9ef9541bee1707e3e45a7
LJunHee <wnsgml4334@naver.com>
2024-11-19 17:57:31 +0900

    11.19 react04

--- react04/src/pages/Depts.js
+++ react04/src/pages/Depts.js
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import { styleTitle } from './Frame';
+import logo from '../logo.svg';
 
 function Depts() {
-    const[list,setList]=useState([]);
+    const[list,setList]=useState(null);
 
     useEffect(()=>{
+
         fetch('http://localhost:8080/dept/')
         .then(e=>e.json())
         .then(json=>{
             const arr = json.map(ele=>{
-                return (<tr>
-                    <td>{ele.deptno}</td>
-                    <td>{ele.dname}</td>
-                    <td>{ele.loc}</td>
+                return (<tr key={ele.deptno}>
+                    <td><Link to={'/dept/detail?deptno='+ele.deptno}>{ele.deptno}</Link></td>
+                    <td><Link to={'/dept/detail?deptno='+ele.deptno}>{ele.dname}</Link></td>
+                    <td><Link to={'/dept/detail?deptno='+ele.deptno}>{ele.loc}</Link></td>
                     </tr>);
             });
             setList(arr);
@@ -31,7 +33,7 @@ function Depts() {
                     <th>loc</th>
                 </tr>
             </thead>
-            <tbody>{list}</tbody>
+            {list?<tbody>{list}</tbody>:<img src={logo}/>}
         </table>
         <p><Link to={'/dept/add'}>입력</Link></p>
         </>
---
+++ react04/src/pages/Detail.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import {styleTitle} from './Frame'
+
+function Dept() {
+    const navigate = useNavigate();
+    const location=useLocation();
+    const [bean,setBean] = useState({deptno:'-',dname:'-',loc:'-'});
+    useEffect(()=>{
+        const deptno=Number(location.search.split('=')[1]);
+        fetch('http://localhost:8080/dept/'+deptno)
+        .then(e=>e.json())
+        .then(e=>{
+            setBean({...e});
+        });
+    },[]);
+  return (
+    <>
+        <h2 style={styleTitle}>Detail page</h2>
+        <form>
+            <div><input name='deptno' value={bean.deptno}/></div>
+            <div><input name='dname' value={bean.dname}/></div>
+            <div><input name='loc' value={bean.loc}/></div>
+        </form>
+    </>
+  )
+}
+
+export default Dept
